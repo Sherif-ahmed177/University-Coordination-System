4cbc4c7f1201afd2133764c085cf7e9193a5b2f0
sherif ahmed <s.ahmed2268@nu.edu.eg>
2025-05-30 02:30:28 +0300

    update front for website

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,39 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using UniversityCoordinationSystem.Models;
+using UniversityApplicationSystem.Services;
 
 namespace UniversityCoordinationSystem.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly StudentService _studentService;
+        private readonly SchoolService _schoolService;
+        private readonly MajorService _majorService;
+        private readonly ApplicationService _applicationService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            StudentService studentService,
+            SchoolService schoolService,
+            MajorService majorService,
+            ApplicationService applicationService)
         {
             _logger = logger;
+            _studentService = studentService;
+            _schoolService = schoolService;
+            _majorService = majorService;
+            _applicationService = applicationService;
         }
 
         public IActionResult Index()
         {
+            ViewBag.StudentCount = _studentService.GetAllStudents().Count();
+            ViewBag.SchoolCount = _schoolService.GetAllSchools().Count();
+            ViewBag.MajorCount = _majorService.GetAllMajors().Count();
+            ViewBag.ApplicationCount = _applicationService.GetAllApplications().Count();
+
             return View();
         }
 
